9262596faff3236f164b1f202948da090dd23fce
Andy <andrewohejiedeogbu@yahoo.com>
2024-04-23 15:10:58 +0100

    white

--- frontendinternship/app/page.tsx
+++ frontendinternship/app/page.tsx
@@ -11,7 +11,7 @@ export default  function Home  () {
 
   return(
     // <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <div className="flex justify-between items-center" style={{ backgroundColor: 'white'}}>
+    <div className="flex justify-between items-center" style={{ backgroundColor: 'white'}} >
      <Rightbar /> 
     <Center />
      <Leftbar /> 
